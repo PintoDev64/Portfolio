2c2a246bf084a57397a7ce4ef8387b72f9f493dd
PintoGamer64 <pintogamer64@gmail.com>
2024-01-11 19:18:13 -0500

    Add Navbar
    Barra de navegacion agregada

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+    en: {
+        'nav.about': 'About',
+        'nav.experience': 'Experience',
+        'nav.projects': 'Projects',
+    },
+    es: {
+        'nav.about': 'Sobre Mi',
+        'nav.experience': 'Experiencia',
+        'nav.projects': 'Proyectos',
+    },
+};
