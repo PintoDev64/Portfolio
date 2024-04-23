fc64573c7693bd0c9cb9bb80534ce908e8aad1dd
PintoGamer64 <pintogamer64@gmail.com>
2024-04-23 12:26:45 -0500

    Branch to deploy on Cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: 'static',
 });
