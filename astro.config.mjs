40fdd9b01224d81133966c1bdce456ecb8c819e1
Joan Sebastian Cardozo Salinas <84690368+PintoGamer64@users.noreply.github.com>
2024-06-05 17:19:25 -0500

    Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
